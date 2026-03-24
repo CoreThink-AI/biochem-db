9ab0262211d89c805f43a8993da3e65e82ce643b
hobs <git@totalgood.com>
2026-03-24 12:04:12 -0700

    eval.py generates summary reports

--- src/biochem/eval.py
+++ src/biochem/eval.py
@@ -1,13 +1,15 @@
 """ Directly queries pubchem API rather (using `src/biochem/pubchem` than local PostgreSQL database (using `src/biochem/db.py`) so that anyone can use it """ 
 from biochem.constants import CID_DIR
-from pyxdameraulevenshtein import normalized_damerau_levenshtein_distance as normalized_distance
-from pathlib import Path
-# TODO: Use https://pubchem.ncbi.nlm.nih.gov/docs/pug-rest#section=URL-based-API to get molecule data in serializable dictionaries rather than unserializable pubchempy objects
-import pubchempy as pc
+from collections import abc
 import json
-import re
 import logging
-
+import re
+import requests
+from pathlib import Path
+import pubchempy as pc
+import pandas as pd
+from pyxdameraulevenshtein import normalized_damerau_levenshtein_distance as normalized_distance
+# TODO: Use https://pubchem.ncbi.nlm.nih.gov/docs/pug-rest#section=URL-based-API to get molecule data in serializable dictionaries rather than unserializable pubchempy objects
 
 
 CRE_CAPITAL_CHAR = re.compile("[A-Z]")
@@ -17,19 +19,19 @@ log = logging.getLogger()
 def get_compound_dict_pubchempy(cid):
     """ FIXME: use HTTP REST instead of pubchempy used here Query PubChem API to retrieve compound record as a dictionary """
     compound = pc.Compound.from_cid(cid)
-    compound_dict = {k: getattr(compound, k) for k in dir(compound) if k in ['_record'] or not k.startswith('_')}
+    compound_dict = {k: v  for (k, v) in compound.to_dict().items() if k in ['_record'] or not k.startswith('_')}
     # compound_dict.update({pubchem2reasoner_dict.get(k, k): getattr(compound, k) for k in dir(compound) if not k.startswith('_')})
-    compound_dict['_record'] = compound._record
+    # compound_dict['_record'] = compound._record
     return compound_dict
 
 
-def get_compound_dict(cid):
-    """ Use HTTP REST to retrieve compound record as a serializable dictionary """
-    compound = pc.Compound.from_cid(cid)
-    compound_dict = {k: getattr(compound, k) for k in dir(compound) if k in ['_record'] or not k.startswith('_')}
-    # compound_dict.update({pubchem2reasoner_dict.get(k, k): getattr(compound, k) for k in dir(compound) if not k.startswith('_')})
-    compound_dict['_record'] = compound._record
-    return compound_dict
+# def get_compound_dict(cid):
+#     """ Use HTTP REST to retrieve compound record as a serializable dictionary """
+#     compound = pc.Compound.from_cid(cid)
+#     compound_dict = {k: getattr(compound, k) for k in dir(compound) if k in ['_record'] or not k.startswith('_')}
+#     # compound_dict.update({pubchem2reasoner_dict.get(k, k): getattr(compound, k) for k in dir(compound) if not k.startswith('_')})
+#     compound_dict['_record'] = compound._record
+#     return compound_dict
 
 
 def make_serializable(obj, force=False, depth=9):
@@ -88,7 +90,7 @@ def get_cid_path_ints(base_dir=CID_DIR):
     return [int(p.name.split('_')[-1]) for p in Path(base_dir).glob('CID_*')]
 
 
-def dict_edit_distances(m, pubchem_molecule):
+def dict_edit_distances(molecule_guess, pubchem_molecule):
     # zip(dir(pubchempy.Compound), response['molecule']))
     map_schema_reasoner2pubchem = {
         'name': 'name',
@@ -97,13 +99,13 @@ def dict_edit_distances(m, pubchem_molecule):
         'yield': 'estimated_yield',
         # 'smiles': 'isomeric_smiles',
     }
-    for k in m:
+    for k in molecule_guess:
         map_schema_reasoner2pubchem[k] = camelcase_to_snake_case(k)
         map_schema_reasoner2pubchem[homogenize(k)] = camelcase_to_snake_case(k)
     
     distances = {}
     annotations ={}
-    for k, guess in m.items():
+    for k, guess in molecule_guess.items():
         if k.startswith('_') and k not in ['_record']:
             continue
         k_pubchem = map_schema_reasoner2pubchem[k]
@@ -111,58 +113,39 @@ def dict_edit_distances(m, pubchem_molecule):
             continue
         truth = pubchem_molecule[k_pubchem]
         distances[k_pubchem] = normalized_distance(str(truth), str(guess))
+        annotations[k + "_normalized_edit_distance"] = distances[k_pubchem]
+        annotations[k + " "] = str(guess)
         annotations[k + '_'] = truth  # record the truth in the reasoner response dict
-    m.update(annotations)
+    molecule_guess.update(annotations)
     report = dict(
         distances={k: distances[k] for k in sorted(distances.keys())},
-        molecule={k: m[k] for k in sorted(m.keys())},
+        molecule_guess={k: molecule_guess[k] for k in sorted(molecule_guess.keys())},
+        annotations=annotations,
     )
     return report
 
 
-# def dict_edit_distances_pubchem(m, m_):
-#     for k in m_:
-#         if k in m:
-#             r['molecule'][k+'_'] = compound_dict[k]  # record the truth in the reasoner response dict
-#             pubchem_distances[k] = distance(str(compound_dict[k]), str(m[k]))
-#         elif k in r:
-#             r[k+'_'] = compound_dict[k]  # record the truth in the reasoner response dict
-#             pubchem_distances[k] = distance(str(compound_dict[k]), str(r[k]))
-#         else:
-#             continue
-#         r['molecule'][k+'_'] = compound_dict[k]
-#     return r
-
-def make_serializable(d):
-
-
-
-def print_report_summary(report):
+def print_report_summary(report, with_ord=None):
     # print(f'== {cid} == {report["pubchem_report"]['connectivity_smiles']}' + '='*80)
     cid = report['pubchem_report']['cid']
-    print(">>> report['pubchem_report']['cid'])")
-    print(cid)
-    print(f'>>> report = evaluate({cid}, with_ord={with_ord})')
-    print(f">>> report["pubchem_report"]['connectivity_smiles']")
-    print(report["pubchem_report"]['connectivity_smiles'])
-    print(f">>> print(json.dumps(report['distances'], indent=4))")
-    print(json.dumps(report['distances'], indent=4))
+    # print(">>> report['pubchem_report']['cid'])")
+    # print(cid)
+    print(f'#### `evaluate({cid}, with_ord={with_ord})["annotations"]`')
+    print('```json')
+    print(json.dumps(report['annotations'], indent=4))
+    print('```')
+    summary = report.get('summary')
+    if summary:
+        print(f'#### `evaluate({cid}, with_ord={with_ord})["summary"]`')
+        print('```json')
+        print(json.dumps(summary, indent=4))
+        print('```')
+    print()
 
 
 def evaluate(cid=10297, with_ord=None):
     if cid is None:
         return evaluate(cid=get_cid_path_ints(), with_ord=with_ord)
-    # if isinstance(cid, (list, tuple)):
-    #     reports = []
-    #     if with_ord is None or with_ord == 'both':
-    #         with_ord = (False, True)
-    #     for i in cid:
-    #         for wo in with_ord:
-    #             try:
-    #                 reports.append(evaluate(cid=int(i), with_ord=wo))
-    #             except Exception as err:
-    #                 log.error(f'{err}')
-    #     return reports
     if isinstance(cid, (list, tuple)):
         reports = []
         if with_ord is None or with_ord == 'both':
@@ -172,46 +155,43 @@ def evaluate(cid=10297, with_ord=None):
         for i in cid:
             for wo in with_ord:
                 try:
-                    reports.append(evaluate(cid=int(i), with_ord=wo))
+                    report = evaluate(cid=int(i), with_ord=wo)
                 except Exception as err:
                     log.error(f'{err}')
+                reports.append(report)  
+            a = reports[-2]['distances']
+            b = reports[-1]['distances']
+            edit_distance_changes = {k: (b[k] - a[k]) for k in a}
+            reports[-1]['edit_distance_changes'] = edit_distance_changes
+            summary = dict(cid=i, edit_distance_changes=edit_distance_changes, annotations=reports[-1]['annotations'])
+            reports[-1]['summary'] = summary
+            summary_path =  (CID_DIR / f'CID_{i}') / 'base_reasoner+ord.summary.json'
+            with open(summary_path, 'wt') as fout:
+                json.dump(summary, fout, indent=4)
         return reports
 
-    reasoning =  (CID_DIR / f'CID_{cid}' / 'base_reasoner.json')
+    reasoning_path = (CID_DIR / f'CID_{cid}' / 'base_reasoner.json')
     if with_ord:
-        reasoning =  reasoning.parent / 'base_reasoner+ord.json'
-    if isinstance(reasoning, (Path, str)):
-        reasoning = json.load(open(reasoning))
+        reasoning_path =  reasoning_path.parent / 'base_reasoner+ord.json'
+    reasoning = json.load(open(reasoning_path))
     cot = reasoning['response']
     if cid is None:
         cid = reasoning['cid']
     cid = int(cid)
-    
-    # map_schema_reasoner2pubchem = [(v, k) for (k, v) in map map_schema_pubchem2reasoner]
-    compound_dict = get_compound_dict(cid=cid)
-    # print(json.dumps(compound_dict, indent=4))
+
+    compound_dict = get_compound_dict_pubchempy(cid=cid)
     report = dict_edit_distances(cot['molecule'], compound_dict)
     report['pubchem_report'] = compound_dict
     report['cot'] = {k: cot[k] for k in sorted(cot)}
     report['cot']['prompts'] = prompts = [{k: p[k] for k in sorted(p)} for p in report['cot']['prompts']]
     responses = []
-
-    # for i, p in enumerate(prompts):
-    #     print(i, type(p), len(p), p.keys())
-    #     r = p['response']
-    #     responses.append({k: r[k] for k in sorted(r)})
-    #     # r = json.dumps(p['response'], indent=4)
     report['reasoner_response'] = json.loads(prompts[-1]['response'])
     final_response = json.loads(report['cot']['prompts'][-1]['response'])
     # print(json.dumps(report, indent=4))
-    print(f'== {cid} == {report["pubchem_report"]['connectivity_smiles']}' + '='*80)
-    print(">>> report['pubchem_report']['cid'])")
-    print(report['pubchem_report']['cid'])
-    print(f'>>> report = evaluate({cid}, with_ord={with_ord})')
-    print(f">>> report["pubchem_report"]['connectivity_smiles']")
-    print(report["pubchem_report"]['connectivity_smiles'])
-    print(f">>> print(json.dumps(report['distances'], indent=4))")
-    print(json.dumps(report['distances'], indent=4))
+    if with_ord:
+        print_report_summary(report, with_ord=with_ord)
+    with open(Path(reasoning_path).with_suffix('.report.json'), 'wt') as fout:
+        json.dump(report, fout, indent=4)
     return report
 
 
@@ -233,10 +213,18 @@ def review_cot(cid):
         print(p['user_prompt'])
 
 
-
 if __name__ == '__main__':
-    report_baseline = evaluate(None)
-    report_ord = evaluate(None, with_ord=True)
+    reports = evaluate(None)
+    changes = {r['summary']['cid']: r['summary']['edit_distance_changes'] for r in reports if 'summary' in r}
+    df = pd.DataFrame(changes).T
+    with open(CID_DIR / f'evaluation_report_{len(reports)/2}_cids.json', 'wt') as fout:
+        json.dump(reports, fout, indent=4)
+    with open(CID_DIR / f'edit_distance_changes_{len(df)}_cids.csv', 'wt') as fout:
+        df.to_csv(fout)
+    print(df.describe())
+
+
+
     
     # cid_dirs = list(CID_DIR.glob('CID_1029*'))
     # pairs = []
